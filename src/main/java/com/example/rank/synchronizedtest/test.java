fa80b29f5634649316905a83a87e01aefa5ae170
陶帅 <taoshuai20221201@zhaoyang.cn>
2022-12-26 14:46:53 +0800

    word测试

---
+++ src/main/java/com/example/rank/synchronizedtest/test.java
@@ -0,0 +1,37 @@
+package com.example.rank.synchronizedtest;
+
+/**
+ * @ClassName test
+ * @Description
+ * @Author ts
+ * @Date 2022/12/17 10:20
+ * @Version 1.0
+ */
+public class test {
+
+
+
+
+    public void test01(){
+
+        synchronized(this){
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public synchronized void test02(){
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
