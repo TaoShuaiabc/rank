ed182cf61d662b135e4d99b3a3f7b801951a45d7
陶帅 <taoshuai20221201@zhaoyang.cn>
2022-12-27 11:13:46 +0800

    rabbitmq模块创建成功

---
+++ rabbitmq/src/main/java/com/shuaitao/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,16 @@
+package com.shuaitao.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ASUS
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+
+}
