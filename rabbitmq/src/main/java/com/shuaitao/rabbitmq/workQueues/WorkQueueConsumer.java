f950f7846115a0733ff7888193c78847efe00622
陶帅 <taoshuai20221201@zhaoyang.cn>
2022-12-27 14:42:27 +0800

    工作线程测试完毕-必须启动多个，启动一个创建多个线程并未达到预期效果

--- rabbitmq/src/main/java/com/shuaitao/rabbitmq/workQueues/WorkQueueConsumer.java
+++ rabbitmq/src/main/java/com/shuaitao/rabbitmq/workQueues/WorkQueueConsumer.java
@@ -1,5 +1,11 @@
 package com.shuaitao.rabbitmq.workQueues;
 
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.shuaitao.rabbitmq.Utils.RabbitMQUtils;
+import com.shuaitao.rabbitmq.constant.QueueConstant;
+
 /**
  * @ClassName WorkQueueConsumer
  * @Description 工作队列消费者
@@ -8,4 +14,25 @@ package com.shuaitao.rabbitmq.workQueues;
  * @Version 1.0
  */
 public class WorkQueueConsumer {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        Channel channel = RabbitMQUtils.getChannel();
+        channel.queueDeclare(QueueConstant.QUEUE_NAME,false,false,false,null);
+
+        //接收消息
+        DeliverCallback deliverCallback = (consumerTag, message)->{
+            System.out.println(new String(message.getBody()));
+        };
+
+        //消息取消消费的回调
+        CancelCallback cancelCallback = (consumerTag) ->{
+            System.out.println("取消消费");
+        };
+
+        channel.basicConsume(QueueConstant.QUEUE_NAME,true,deliverCallback,cancelCallback);
+
+    }
 }
--- rabbitmq/src/main/java/com/shuaitao/rabbitmq/workQueues/WorkQueueProducer.java
+++ rabbitmq/src/main/java/com/shuaitao/rabbitmq/workQueues/WorkQueueProducer.java
@@ -1,5 +1,11 @@
 package com.shuaitao.rabbitmq.workQueues;
 
+import com.rabbitmq.client.Channel;
+import com.shuaitao.rabbitmq.Utils.RabbitMQUtils;
+import com.shuaitao.rabbitmq.constant.QueueConstant;
+
+import java.util.Scanner;
+
 /**
  * @ClassName WorkQueueProducer
  * @Description 工作队列生产者
@@ -10,7 +16,17 @@ package com.shuaitao.rabbitmq.workQueues;
 public class WorkQueueProducer {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMQUtils.getChannel();
+        channel.queueDeclare(QueueConstant.QUEUE_NAME,false,false,false,null);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String message = scanner.next();
+            channel.basicPublish("",QueueConstant.QUEUE_NAME,null,message.getBytes());
+            System.out.println("消息发送完成:" + message);
+        }
 
     }
 
