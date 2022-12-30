f2eaa97881cd123d547a4bb5ce60e495b81153cc
陶帅 <taoshuai20221201@zhaoyang.cn>
2022-12-30 10:39:52 +0800

    算法题

---
+++ src/main/java/com/example/rank/likou/ShowMeBug.java
@@ -0,0 +1,35 @@
+package com.example.rank.likou;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName ShowMeBug
+ * @Description
+ * @Author ts
+ * @Date 2022/12/29 17:35
+ * @Version 1.0
+ */
+public class ShowMeBug {
+
+    /**
+     *
+     max[0][0] max[1][0]
+     max[0][1] max[1][1]
+     max[0][2] max[1][2]
+     */
+
+    public static void main(String[] args) {
+
+        int[][] max = {{1,2,3},{4,5,6},{7,8,9}};
+
+        int[][] ins  = new int[max[0].length][max.length];
+
+        for (int i=0;i<max.length;i++){
+            for (int j=0;j<max[i].length;j++){
+                ins[j][i] = max[i][j];
+            }
+        }
+
+        System.out.println(Arrays.deepToString(ins));
+    }
+}
