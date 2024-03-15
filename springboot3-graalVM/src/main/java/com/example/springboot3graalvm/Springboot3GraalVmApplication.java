35af0d7e63f14468b9101b59def074e7da161482
taoshuai <542564264@qq.com>
2024-03-15 12:29:31 +0800

    GraalVM

---
+++ springboot3-graalVM/src/main/java/com/example/springboot3graalvm/Springboot3GraalVmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3GraalVmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3GraalVmApplication.class, args);
+	}
+
+}
---
+++ springboot3-graalVM/src/test/java/com/example/springboot3graalvm/Springboot3GraalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3GraalVmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
