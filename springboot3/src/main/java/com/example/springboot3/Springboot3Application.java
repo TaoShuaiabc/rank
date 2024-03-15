e6dc1333d4fd33e586eb4578fa759f19606ef77c
taoshuai <542564264@qq.com>
2024-03-15 12:09:33 +0800

    创建算法模块

--- springboot3/src/main/java/com/example/springboot3/Springboot3Application.java
+++ springboot3/src/main/java/com/example/springboot3/Springboot3Application.java
@@ -1,14 +1,10 @@
 package com.example.springboot3;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Springboot3Application {
 
     public static void main(String[] args) {
         System.out.println("hello word!");
-        SpringApplication.run(Springboot3Application.class, args);
     }
 
 }
