7698967eda4e192ce328ad02d068586b1d2799e4
taoshuai <542564264@qq.com>
2024-04-25 11:05:23 +0800

    美团leaf练习

---
+++ springboot-leaf/src/main/java/com/leaf/springbootleaf/SpringbootLeafApplication.java
@@ -0,0 +1,13 @@
+package com.leaf.springbootleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLeafApplication.class, args);
+    }
+
+}
---
+++ springboot-leaf/src/test/java/com/leaf/springbootleaf/SpringbootLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaf.springbootleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLeafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
