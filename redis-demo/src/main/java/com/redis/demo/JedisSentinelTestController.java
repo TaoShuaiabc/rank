2fce5281b6c608175d65099193fdd78150db3cf4
taoshuai <542564264@qq.com>
2024-07-11 08:24:58 +0800

    redid

--- redis-demo/src/main/java/com/redis/demo/JedisSentinelTestController.java
+++ redis-demo/src/main/java/com/redis/demo/JedisSentinelTestController.java
@@ -2,6 +2,9 @@ package com.redis.demo;
 
 
 import jakarta.annotation.Resource;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,13 @@ public class JedisSentinelTestController {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private Redisson redisson;
 
 
     @RequestMapping("/test_sentinel")
@@ -31,4 +41,30 @@ public class JedisSentinelTestController {
         }
 
     }
+
+    @RequestMapping("/cluster")
+    public void testCluster() throws InterruptedException {
+        stringRedisTemplate.opsForValue().set("stock", "300");
+        System.out.println(stringRedisTemplate.opsForValue().get("stock"));
+
+    }
+
+    //分布式锁测试
+    @RequestMapping("/key")
+    public String key(){
+        RLock lock = redisson.getLock("1011");
+        lock.lock();
+        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+        if (stock>0){
+            int realStock = stock - 1 ;
+            stringRedisTemplate.opsForValue().set("stock",realStock+"");
+            System.out.println(realStock);
+        }else {
+            System.out.println("扣减库存失败");
+        }
+        lock.unlock();
+        return "200";
+    }
+
+
 }
--- redis-demo/src/main/java/com/redis/demo/RedisDemoApplication.java
+++ redis-demo/src/main/java/com/redis/demo/RedisDemoApplication.java
@@ -1,14 +1,25 @@
 package com.redis.demo;
 
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
+    }
+
+    @Bean
+    public Redisson redisson(){
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.219.88:6379").setDatabase(0);
+        return (Redisson) Redisson.create(config);
     }
 
 }
