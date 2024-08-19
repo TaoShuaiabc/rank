212c660f1dbecb0d2a6012428d2ea2f9e6885415
taoshuai <542564264@qq.com>
2024-08-19 19:08:36 +0800

    并发编程

--- thread-demo/src/main/java/com/design/threaddemo/test7/Test03.java
+++ thread-demo/src/main/java/com/design/threaddemo/test7/Test03.java
@@ -3,9 +3,10 @@ package com.design.threaddemo.test7;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Test02 {
+public class Test03 {
     //是否分配女朋友
     static boolean flag = false;
+    static boolean money = false;
     //锁对象
     static Object key = new Object();
 
@@ -21,7 +22,7 @@ public class Test02 {
      * 痛点：
      *   notify()只会随机叫醒一个
      * 再次改进：
-     *   使用notifyAll(),可以唤醒所有 参考test3
+     *   使用notifyAll(),可以唤醒所有  参考test3
      */
     public static void main(String[] args) throws InterruptedException {
 
@@ -34,14 +35,14 @@ public class Test02 {
                 } else {
                     try {
                         log.info("jack发现没有女朋友");
-                        log.info("jack开始罢工5秒钟");
+                        log.info("jack开始休息");
                         //如果没有分配女朋友，则jack去休息
                         key.wait();
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                     //睡眠五秒钟后，判断是否分配
-                    log.info("jack睡醒了");
+                    log.info("jack被老板叫醒");
                     if (flag) {
                         log.info("发现老板分配了女朋友");
                         log.info("jack开始工作");
@@ -56,23 +57,51 @@ public class Test02 {
 
         },"jack").start();
 
+
+        new Thread(() -> {
+            synchronized (key) {
+                //如果有女朋友
+                if (money) {
+                    log.info("rose开始工作");
+                } else {
+                    try {
+                        log.info("rose发现没有钱");
+                        log.info("rose开始休息");
+                        //如果没有分配女朋友，则jack去休息
+                        key.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    log.info("rose被唤醒了");
+                    if (money) {
+                        log.info("rose发现老板给了钱");
+                        log.info("rose开始工作");
+                    }else {
+                        log.info("rose直接回家");
+                    }
+                }
+
+
+            }
+
+
+        },"rose").start();
+
+
+        Thread.sleep(1000);
         new Thread(()->{
             synchronized (key) {
                 log.info("老板分配了一个女朋友给jack");
+                log.info("老板发了工资给rose");
                 flag = true;
-                key.notify();
+                money = true;
+                //只会随机叫醒一个
+                //key.notify();
+                //会同时唤醒jack和rose
+                key.notifyAll();
             }
 
         }).start();
-
-        //创建10个程序员
-        for (int i = 0; i < 10; i++) {
-            new Thread(()->{
-                synchronized (key) {
-                    log.info(Thread.currentThread().getName()+"程序员开始加班！");
-                }
-            },i+"").start();
-        }
 
 
     }
